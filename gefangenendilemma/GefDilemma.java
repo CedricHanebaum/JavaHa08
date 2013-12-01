6c454ed5096b9026a5da16a0d99d27cf8bca75f4
Cedric Hanebaum <Chanebaum@gmx.de>
2013-12-01 17:44:01 +0100

    finished turn method

--- gefangenendilemma/GefDilemma.java
+++ gefangenendilemma/GefDilemma.java
@@ -1,19 +1,18 @@
 package gefangenendilemma;
 
 public class GefDilemma {
-	
+
 	private Player player1;
 	private Player player2;
-	
-	
+
 	public GefDilemma(GefStrategie str1, GefStrategie str2) {
 		super();
 		this.player1 = new Player(str1);
 		this.player2 = new Player(str2);
 	}
-	
-	public void spiele(int n){
-		for(int i = 0; i < n; i++){
+
+	public void spiele(int n) {
+		for (int i = 0; i < n; i++) {
 			this.turn();
 		}
 	}
@@ -22,16 +21,25 @@ public class GefDilemma {
 		boolean p1, p2;
 		p1 = player1.getNextDecision();
 		p2 = player2.getNextDecision();
-		
-		if(p1 && p2){
-			player1.addPoints(2);
-			player2.addPoints(2);
+
+		if (p1 ^ p2) {
+			if (p1) {
+				player1.addPoints(5);
+			} else {
+				player2.addPoints(5);
+			}
+		} else {
+			if (p1) {
+				player1.addPoints(2);
+				player2.addPoints(2);
+			} else {
+				player1.addPoints(5);
+				player2.addPoints(5);
+			}
 		}
 		
-		if(!p1 && !p2){
-			player1.addPoints(5);
-			player2.addPoints(5);
-		}
+		player1.setOpponentsLastDecision(p2);
+		player2.setOpponentsLastDecision(p1);
 	}
 
 }
